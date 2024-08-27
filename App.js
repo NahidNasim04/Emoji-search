c405e2670813cb170614a1511bd8acde690a4c0a
Nahid Nasim <148656358+NahidNasim04@users.noreply.github.com>
2024-08-27 21:12:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmojiSearch from './EmojiSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <EmojiSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ emojiData.js
@@ -0,0 +1,12 @@
+export const emojiData = [
+    { emoji: "😊", name: "Smiling Face with Smiling Eyes" },
+    { emoji: "😂", name: "Face with Tears of Joy" },
+    { emoji: "❤️", name: "Red Heart" },
+    { emoji: "👍", name: "Thumbs Up" },
+    { emoji: "🔥", name: "Fire" },
+    { emoji: "🎉", name: "Party Popper" },
+    { emoji: "😢", name: "Crying Face" },
+    { emoji: "💪", name: "Flexed Biceps" },
+    { emoji: "🙏", name: "Folded Hands" },
+    { emoji: "🎂", name: "Birthday Cake" },
+  ];
